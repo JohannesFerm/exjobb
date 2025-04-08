38ba0face78f67cf63b9c531d32f1f7769b26678
JohannesFerm <johannesferm1@gmail.com>
2025-04-08 11:30:29 +0200

    Changed some pose_drive stuff, added speed command

--- data_collection/data_collection/data_collection.py
+++ data_collection/data_collection/data_collection.py
@@ -10,15 +10,21 @@ import os
 import numpy as np
 from hqv_public_interface.msg import MowerImu
 import datetime 
+import csv
 
 
 class DataCollectorNode(Node):
     def __init__(self):
         super().__init__('DataCollectorNode')
         self.recording = False
+        self.audioStream = None
+        self.audioFile = None
 
         self.imuSub = self.create_subscription(MowerImu, '/hqv_mower/imu0/orientation', self.IMUCallback, 10)
+        self.imuFile = None
         self.orientation = np.zeros(3)
+        self.lastCallbackTime = time.time()
+        self.sampleFolder = None
 
         #Setup flask server stuff
         self.app = Flask(__name__, template_folder="templates")
@@ -47,62 +53,60 @@ class DataCollectorNode(Node):
         flask_thread.start()
 
     def IMUCallback(self, msg):
-        self.orientation = np.array([msg.roll, msg.pitch, msg.yaw])
+        currentTime = time.time()
+
+        
+        if currentTime - self.lastCallbackTime >= 1.0:
+            self.lastCallbackTime = currentTime 
+            self.orientation = np.array([msg.roll, msg.pitch, msg.yaw])
+
+            if self.imuFile:
+                self.imuFile.write(f"{currentTime},{self.orientation[0]},{self.orientation[1]},{self.orientation[2]}\n")
+
 
     def run_flask(self):
         self.app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
 
     def record(self, label):
         audio = pyaudio.PyAudio()
-
         FORMAT = pyaudio.paInt16
         CHANNELS = 1
         RATE = 44100
         CHUNK = 512
-        RECORD_SECONDS = 1
-
-        sample = 0
 
         dateStr = datetime.datetime.now().strftime("%Y-%m-%d")
-        dirPath = f"dataset/{dateStr}/{label}"
-        os.makedirs(dirPath, exist_ok=True) 
+        self.sampleFolder = f"dataset/{dateStr}/{label}"
+        os.makedirs(self.sampleFolder, exist_ok=True)
 
-        #Record audio and imu data
+        audioPath = os.path.join(self.sampleFolder, f"audio{time.time()}.wav")
+        imuPath = os.path.join(self.sampleFolder, f"imu{time.time()}.csv")
+
+        self.audioFile = wave.open(audioPath, 'wb')
+        self.audioFile.setnchannels(CHANNELS)
+        self.audioFile.setsampwidth(audio.get_sample_size(FORMAT))
+        self.audioFile.setframerate(RATE)
+
+        self.imuFile = open(imuPath, 'w', newline='')
+        imuWriter = csv.writer(self.imuFile)
+        imuWriter.writerow(["timestamp", "roll", "pitch", "yaw"])
+
+        self.audioStream = audio.open(format=FORMAT, channels=CHANNELS,
+                                       rate=RATE, input=True,
+                                       frames_per_buffer=CHUNK)
+
         while self.recording:
-            
-            sampleName = f"sample_{int(time.time() * 1000)}_{sample+1:03d}"
-            sampleFolder = os.path.join(dirPath, sampleName)
-            os.makedirs(sampleFolder, exist_ok=True)
+            data = self.audioStream.read(CHUNK)
+            self.audioFile.writeframes(data)
 
-            stream = audio.open(format=FORMAT, channels=CHANNELS,
-                            rate=RATE, input=True,
-                            frames_per_buffer=CHUNK)
-            frames = []
-            
-            audioPath = os.path.join(sampleFolder, f"{sampleName}.wav")
-            imuPath = os.path.join(sampleFolder, f"{sampleName}.npy")
-
-            for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-                data = stream.read(CHUNK)
-                frames.append(data)
-            
-            stream.stop_stream()
-            stream.close()
-
-            #Save the audio data
-            waveFile = wave.open(audioPath, 'wb')
-            waveFile.setnchannels(CHANNELS)
-            waveFile.setsampwidth(audio.get_sample_size(FORMAT))
-            waveFile.setframerate(RATE)
-            waveFile.writeframes(b''.join(frames))
-            waveFile.close()
-
-            #Save the IMU data
-            np.save(imuPath, self.orientation)
-            
-            sample += 1    
-
+        self.audioStream.stop_stream()
+        self.audioStream.close()
+        self.audioFile.close()
+        self.imuFile.close()
         audio.terminate()
+
+        self.audioStream = None
+        self.audioFile = None
+        self.imuFile = None
 
 def main(args=None):
     rclpy.init(args=args)
--- pose_drive/pose_drive/pose_drive.py
+++ pose_drive/pose_drive/pose_drive.py
@@ -16,16 +16,27 @@ class PoseDrive(Node):
 
         self.app = Flask(__name__)
         self.command = ""
+        self.speed = 0.33
 
         @self.app.route('/command', methods=['POST'])
         def receive_message():
             # Get the message from the POST request
             self.command = request.json.get('command')  # Match the key used in the client
             print(f"Received command: {self.command}")
-            self.motorControl(self.command)
+            self.motorControl()
             
             return {"status": "success", "message": "Message received"}, 200
         
+        @self.app.route('/speed', methods=['POST'])
+        def speedMsg():
+            msg = request.json.get('speed')
+            if msg == "slow":
+                self.speed = 0.33
+            elif msg == "fast":
+                self.speed = 2.0
+                
+            return {"status": "success", "message": "Message received"}, 200
+
         flask_thread = threading.Thread(target=self.run_flask)
         flask_thread.daemon = True
         flask_thread.start()
@@ -33,45 +44,38 @@ class PoseDrive(Node):
     def run_flask(self):
         self.app.run(debug=False, host='0.0.0.0', port=5000)
 
-    def motorControl(self, msg): #Inspiration from the provided remote_drive_node.py
+    def motorControl(self): #Inspiration from the provided remote_drive_node.py
         if self.command == "left":
             msg = RemoteDriverDriveCommand()
             msg.header.stamp = self.get_clock().now().to_msg()
-            msg.speed = 0.33
+            msg.speed = self.speed
             msg.steering = 2.0
             self.drive_publisher.publish(msg)
         elif self.command == "right":
             msg = RemoteDriverDriveCommand()
             msg.header.stamp = self.get_clock().now().to_msg()
-            msg.speed = 0.33
+            msg.speed = self.speed
             msg.steering = -2.0
             self.drive_publisher.publish(msg)
         elif self.command == "closedhand":
             msg = RemoteDriverDriveCommand()
             msg.header.stamp = self.get_clock().now().to_msg()
-            msg.speed = 0.33
+            msg.speed = self.speed
             msg.steering = 0.0
             self.drive_publisher.publish(msg)
         elif self.command == "openhand":
             msg = RemoteDriverDriveCommand()
             msg.header.stamp = self.get_clock().now().to_msg()
-            msg.speed = -0.33
+            msg.speed = -self.speed
             msg.steering = 0.0
             self.drive_publisher.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)
-
     motorController = PoseDrive()
-
     rclpy.spin(motorController)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
     motorController.destroy_node()
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
