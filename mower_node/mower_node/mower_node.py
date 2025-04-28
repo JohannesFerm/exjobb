02f7324c5ab378617a0e6046d46b805769e8028c
JohannesFerm <johannesferm1@gmail.com>
2025-04-28 11:02:30 +0200

    Put inference in its own thread, solving the issue of there being gaps between audio clips. Cleaned up some code

--- mower_node/mower_node/mower_node.py
+++ mower_node/mower_node/mower_node.py
@@ -16,7 +16,20 @@ import torch
 from mower_node.model import MultimodalModel
 import os
 import librosa
+import contextlib
+import sys
 
+#Helper to avoid ALSA warnings
+@contextlib.contextmanager
+def suppressStderr():
+    with open(os.devnull, 'w') as devnull:
+        old_stderr_fd = os.dup(2) 
+        os.dup2(devnull.fileno(), 2) 
+        try:
+            yield
+        finally:
+            os.dup2(old_stderr_fd, 2)
+            os.close(old_stderr_fd)
 
 class MowerNode(Node):
     def __init__(self):
@@ -24,8 +37,12 @@ class MowerNode(Node):
 
         #For audio
         self.recording = False
-        self.audioStream = None
         self.audioFile = None
+        self.FORMAT = pyaudio.paInt16
+        self.CHANNELS = 1
+        self.RATE = 44100
+        self.CHUNK = 512
+        self.RECORD_SECONDS = 2
 
         #For IMU
         self.imuSub = self.create_subscription(MowerImu, '/hqv_mower/imu0/orientation', self.IMUCallback, 10)
@@ -45,6 +62,20 @@ class MowerNode(Node):
         self.testing = False
         self.model = None
         self.imuBuffer =  [[0.0, 0.0, 0.0] for _ in range(10)]
+
+        #Load the model
+        if self.model is None:
+            #Find the weights
+            model_path = "models/model_weights.pth"
+            
+            NUM_CLASSES = 4 #Should be 5
+            self.model = MultimodalModel(numClasses=NUM_CLASSES)
+            self.model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+            self.model.eval()
+
+        #Librosa uses numba, dummy call to avoid first (real) call taking longer
+        dummyAudio = np.zeros(44100 * 2, dtype=np.float32) 
+        _ = librosa.feature.melspectrogram(y=dummyAudio, sr=44100, n_fft=2048, hop_length=512, n_mels=128)
 
         #Setup flask server stuff
         self.app = Flask(__name__, template_folder="templates")
@@ -76,11 +107,13 @@ class MowerNode(Node):
             threading.Thread(target=self.test, daemon=True).start()
             return jsonify({"status": "Test started"})
         
+        #For stopping the testing
         @self.app.route("/stop-test", methods=["GET"])
         def stopTest():
             self.testing = False
             return jsonify({"status": "Test stopped"})
 
+        #For getting the current prediction to put on screen
         @self.app.route("/prediction", methods=["GET"])
         def getPrediction():
             if self.testing:
@@ -112,12 +145,10 @@ class MowerNode(Node):
     def run_flask(self):
         self.app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
 
+    #Function for data collection
     def record(self, label):
-        audio = pyaudio.PyAudio()
-        FORMAT = pyaudio.paInt16
-        CHANNELS = 1
-        RATE = 44100
-        CHUNK = 512
+        with suppressStderr():
+            audio = pyaudio.PyAudio()
 
         #Define directory
         sampleId = f"sample_{int(time.time())}"
@@ -131,9 +162,9 @@ class MowerNode(Node):
 
         #Setup audio
         self.audioFile = wave.open(audioPath, 'wb')
-        self.audioFile.setnchannels(CHANNELS)
-        self.audioFile.setsampwidth(audio.get_sample_size(FORMAT))
-        self.audioFile.setframerate(RATE)
+        self.audioFile.setnchannels(self.CHANNELS)
+        self.audioFile.setsampwidth(audio.get_sample_size(self.FORMAT))
+        self.audioFile.setframerate(self.RATE)
 
         #Setup IMU
         self.imuFile = open(imuPath, 'w', newline='')
@@ -146,82 +177,66 @@ class MowerNode(Node):
         gpsWriter.writerow(["timestamp", "latitude", "longitude"])
 
         #Record audio
-        self.audioStream = audio.open(format=FORMAT, channels=CHANNELS,
-                                       rate=RATE, input=True,
-                                       frames_per_buffer=CHUNK)
+        stream = audio.open(format=self.FORMAT, channels=self.CHANNELS,
+                                       rate=self.RATE, input=True,
+                                       frames_per_buffer=self.CHUNK)
         while self.recording:
-            data = self.audioStream.read(CHUNK)
+            data = stream.read(self.CHUNK)
             self.audioFile.writeframes(data)
 
         #Clean up
-        self.audioStream.stop_stream()
-        self.audioStream.close()
+        stream.stop_stream()
+        stream.close()
         self.audioFile.close()
         self.imuFile.close()
         self.gpsFile.close()
         audio.terminate()
 
-        self.audioStream = None
         self.audioFile = None
         self.imuFile = None
         self.gpsFile = None
     
+    #Function for testing the model
     def test(self):
+        with suppressStderr():
+            audio = pyaudio.PyAudio()
 
-        #Load the model
-        if self.model is None:
+        stream = audio.open(format=self.FORMAT, channels=self.CHANNELS,
+                            rate=self.RATE, input=True,
+                            frames_per_buffer=self.CHUNK)
 
-            #Find the weights
-            model_path = "models/model_weights.pth"
-            
-            NUM_CLASSES = 4 #Should be 5
-            self.model = MultimodalModel(numClasses=NUM_CLASSES)
-            self.model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
-            self.model.eval()
-
-        #Setup for audio
-        audio = pyaudio.PyAudio()
-        FORMAT = pyaudio.paInt16
-        CHANNELS = 1
-        RATE = 44100
-        CHUNK = 512
-        RECORD_SECONDS = 2
         while self.testing:
-
-            start = time.time()
-            #Record audio (2 seconds)
-            frames = []
-            stream = audio.open(format=FORMAT, channels=CHANNELS,
-                                       rate=RATE, input=True,
-                                       frames_per_buffer=CHUNK)
-            
-            for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-                data = stream.read(CHUNK)
+            #Record audio
+            frames = []            
+            for i in range(0, int(self.RATE / self.CHUNK * self.RECORD_SECONDS)):
+                data = stream.read(self.CHUNK)
                 frames.append(data)
 
-            stream.stop_stream()
-            stream.close()
-
-            #Preprocess audio, convert to spectrogram
-            soundClip = np.frombuffer(b''.join(frames), dtype=np.int16).astype(np.float32) / 32768.0
-            soundClip = librosa.feature.melspectrogram(y=soundClip, sr=RATE, n_fft=2048, hop_length=512, n_mels=128)
-            soundClip = librosa.power_to_db(soundClip, ref=np.max)
-
-            #Tensors for the model
-            audioTensor = torch.tensor(soundClip, dtype=torch.float32).unsqueeze(0).unsqueeze(1)
-            imuTensor = torch.tensor(np.stack(self.imuBuffer), dtype=torch.float32).unsqueeze(0)
-            
-            output = self.model.forward(audioTensor, imuTensor)
-
-            _, pred = torch.max(output, 1)
-            self.prediction = pred.item() #Set output on the server
-
-            stop = time.time()
-            print(f"TIME: {start - stop}")
+            #Start a thread for inference
+            threading.Thread(target=self.infer, args=(frames,)).start()
         
+        #Clean up
+        stream.stop_stream()
+        stream.close()
         audio.terminate()
         self.imuBuffer =  [[0.0, 0.0, 0.0] for _ in range(10)]
         self.prediction = None   
+    
+    #Function for doing inference
+    def infer(self, frames):
+        RATE = 44100
+        soundClip = np.frombuffer(b''.join(frames), dtype=np.int16).astype(np.float32) / 32768.0
+        soundClip = librosa.feature.melspectrogram(y=soundClip, sr=RATE, n_fft=2048, hop_length=512, n_mels=128)
+        soundClip = librosa.power_to_db(soundClip, ref=np.max)
+
+        #Tensors for the model
+        audioTensor = torch.tensor(soundClip, dtype=torch.float32).unsqueeze(0).unsqueeze(1)
+        imuTensor = torch.tensor(np.stack(self.imuBuffer), dtype=torch.float32).unsqueeze(0)
+        
+        output = self.model.forward(audioTensor, imuTensor)
+
+        _, pred = torch.max(output, 1)
+        self.prediction = pred.item() #Set output on the server
 
 def main(args=None):
     rclpy.init(args=args)
