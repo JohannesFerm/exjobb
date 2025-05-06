ccf3ee33973ede3f5edce04ab73e99a55dd75965
JohannesFerm <johannesferm1@gmail.com>
2025-05-06 15:24:58 +0200

    Started with multiprocessing for saving inference time data and map

--- mower_node/mower_node/mower_node.py
+++ mower_node/mower_node/mower_node.py
@@ -20,6 +20,8 @@ import os
 import librosa
 import contextlib
 from geopy.distance import geodesic
+from multiprocessing import Process, Queue
+import json
 
 #Helper to avoid ALSA warnings
 @contextlib.contextmanager
@@ -32,6 +34,47 @@ def suppressStderr():
         finally:
             os.dup2(old_stderr_fd, 2)
             os.close(old_stderr_fd)
+
+#Function that saves inference data when map building
+def dataSaver(queue, dir):
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    sampleDir  = os.path.join(dir, f"sample_{timestamp}")
+    os.makedirs(sampleDir, exist_ok=True)
+
+    sampleFile = os.path.join(sampleDir, "data.csv")
+    with open(sampleFile, 'w', newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(["spectrogram", "imu", "gps", "label"])
+        while True:
+            sample = queue.get()
+            
+            #If stop save the map
+            if isinstance(sample, dict) and sample.get("type") == "STOP":
+                mapData = sample.get("map")
+                if mapData:
+                    mapPath = os.path.join(sampleDir, "map.json")
+                    with open(mapPath, "w") as mf:
+                        json.dump(mapData, mf, indent=2)
+                break
+            
+            #Save rows
+            elif isinstance(sample, dict): 
+                writer.writerow([sample["spectrogram"], sample["imu"], sample["gps"], sample["label"]])
+
+
+#Function that starts the data saving process
+def initDataSaverProcess(dir = "map_logs"):
+    os.makedirs(dir, exist_ok=True)
+    queue = Queue()
+    p = Process(target=dataSaver, args=(queue, dir))
+    p.start()
+    return queue, p
+
+#Function that stops the data saving process
+def stopDataSaverProcess(queue, process):
+    queue.put("STOP")
+    process.join()
+
 
 class MowerNode(Node):
     def __init__(self):
@@ -286,6 +329,7 @@ class MowerNode(Node):
         if self.mapBuilding:
             currentPos = self.pos #Use this line so that the GPS position isn't affected (updated) by the inference taking half a second
                                     #If the timing is bad it might happen that the GPS position from the callback belongs to the next clip without this line
+        imuBufferCur = self.imuBuffer #Avoid same issue as GPS
 
         numpyWheel0 = np.array(self.wheel0buffer)
         numpyWheel1 = np.array(self.wheel1buffer)
@@ -307,7 +351,7 @@ class MowerNode(Node):
 
         #Tensors for the model
         audioTensor = torch.tensor(soundClip, dtype=torch.float32).unsqueeze(0).unsqueeze(1)
-        imuTensor = torch.tensor(np.stack(self.imuBuffer), dtype=torch.float32).unsqueeze(0)
+        imuTensor = torch.tensor(np.stack(imuBufferCur), dtype=torch.float32).unsqueeze(0)
         
         output = self.model.forward(audioTensor, imuTensor)
 
